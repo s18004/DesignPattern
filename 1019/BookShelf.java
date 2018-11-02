9c45ec3713cc6c0c6e840e82b8c876c1ba330fde
s18004 <39285176+s18004@users.noreply.github.com>
2018-11-02 12:40:16 +0900

    try commit

---
+++ 1019/BookShelf.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookShelf implements Aggregate {
+  private List<Book> books;
+  public BookShelf(int maxsize) {
+    this.books = new ArrayList<Book>(maxsize);
+  }
+
+  public Book getBookAt(int index) {
+    return books.get(index);
+  }
+
+  public void appendBook(Book book) {
+    this.books.add(book);
+  }
+
+  public int getLength() {
+    return this.books.size();
+  }
+
+  @Override
+  public Iterator iterator() {
+    return new BookShelfIterator(this);
+  }
+}
---
+++ 1019/BookShelfIterator.java
@@ -0,0 +1,24 @@
+public class BookShelfIterator implements Iterator {
+  private BookShelf bookShelf;
+  private int index;
+  BookShelfIterator(BookShelf bookShelf) {
+    this.bookShelf = bookShelf;
+    this.index = 0;
+  }
+
+  @Override
+  public boolean hasNext() {
+    if (index < bookShelf.getLength()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public Object next() {
+    Book book = bookShelf.getBookAt(index);
+    index++;
+    return book;
+  }
+}
